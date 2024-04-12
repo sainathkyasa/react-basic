cb2a09c6512d93d3bc6084a26d25bf05d9916019
Sainath K. <sainath.kyasa@gramener.org>
2024-04-12 11:51:09 +0530

    added logo

--- App.js
+++ App.js
@@ -1,14 +1,20 @@
 import React from '../project1/node_modules/react';
-import ReactDOM from '../project1/node_modules/react-dom';
+import ReactDOM from '../project1/node_modules/react-dom/client';
+import logo from '../project1/sfc_logo.png';
 
 // const heading = React.createElement("h1", {className: "heading"}, "Hello World! using React");
-
+const Logo = ()=>{
+    return (
+    <img src={logo} alt='logo' height="100" />
+)
+}
 // Functional component
 const  App = () => {
     return (
         <>
-            <div>from Jsx</div>
-            <div>second line</div>
+        <Logo />
+            <div>Sainath Fried Chicken</div>
+            {/* <div>second line</div> */}
         </>
     )
 }
